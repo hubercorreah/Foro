f7318d27a1020c3c406d63e75ad610b16cba9213
hubercorreah <huber.correa@hotmail.com>
2017-11-22 23:08:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kellyhuber/foro/vista/ForoActivity.java
@@ -0,0 +1,15 @@
+package com.example.kellyhuber.foro.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kellyhuber.foro.R;
+
+public class ForoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foro);
+    }
+}
---
+++ app/src/main/java/com/example/kellyhuber/foro/vista/IngresoActivity.java
@@ -0,0 +1,15 @@
+package com.example.kellyhuber.foro.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kellyhuber.foro.R;
+
+public class IngresoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso);
+    }
+}
